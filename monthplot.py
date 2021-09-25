754dee4b40a9614a06d4b7f309e65f66088efce7
caiusseverus <7763268+caiusseverus@users.noreply.github.com>
2021-09-25 21:01:38 +0100

    Update monthplot.py

--- monthplot.py
+++ monthplot.py
@@ -3,9 +3,9 @@ import pandas as pd
 import csv, time
 
 if os.environ.get('DISPLAY') is None: # this variable will not be set when X Server is not running
- import matplotlib as  mplib
+    import matplotlib as  mplib
  # set a  different display back-end to squash the error
- mplib.use('Agg')
+    mplib.use('Agg')
 
 import matplotlib.pyplot as plt
 import seaborn as sns
